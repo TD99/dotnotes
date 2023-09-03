bcbd8ef9cf9e73b8999137d7d0fb5f99273249c8
Tim Dürr <tim@duerrs.ch>
2023-09-03 18:39:12 +0200

    Uploaded final 1.0.0.0

---
+++ DotNotes/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace DotNotes;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
